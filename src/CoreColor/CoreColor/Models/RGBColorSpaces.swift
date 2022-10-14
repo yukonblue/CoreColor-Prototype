1bf42b2658dc5f03383350959111353732f358a5
yukonblue <77284557+yukonblue@users.noreply.github.com>
2022-10-14 15:20:04 -0400

    rgbToXyzMatrix

--- src/CoreColor/CoreColor/Models/RGBColorSpaces.swift
+++ src/CoreColor/CoreColor/Models/RGBColorSpaces.swift
@@ -65,10 +65,9 @@ enum RGBColorSpaces {
 }
 
 struct RGBColorSpaceImpl: RGBColorSpace {
-    
+
     var matrixToXyz: matrix_float3x3 {
-        //        rgbToXyzMatrix(whitePoint, r, g, b)
-        fatalError("TODO")
+        rgbToXyzMatrix(whitePoint: whitePoint, r: r, g: g, b: b)
     }
 
     var matrixFromXyz: matrix_float3x3 {
@@ -80,7 +79,29 @@ struct RGBColorSpaceImpl: RGBColorSpace {
     let name: String
     let whitePoint: WhitePoint
     let transferFunctions: RGBTransferFunctions
+    
     let r: xyY
     let g: xyY
     let b: xyY
+}
+
+// [SMPTE RP 177-1993](http://car.france3.mars.free.fr/Formation%20INA%20HD/HDTV/HDTV%20%202007%20v35/SMPTE%20normes%20et%20confs/rp177.pdf)
+private func rgbToXyzMatrix(whitePoint: WhitePoint, r: xyY, g: xyY, b: xyY) -> matrix_float3x3 {
+    /**
+     primaries = Matrix(
+             r.x, g.x, b.x,
+             r.y, g.y, b.y,
+             r.z, g.z, b.z,
+         )
+     */
+
+    let primaries = matrix_float3x3(columns: (simd_float3(x: r.x, y: r.y, z: r.z),
+                                              simd_float3(x: g.x, y: g.y, z: g.z),
+                                              simd_float3(x: b.x, y: b.y, z: b.z)))
+
+    let wp = whitePoint.chromaticity
+
+    let CP = primaries.inverse * simd_float3(x: wp.X, y: wp.Y, z: wp.Z)
+
+    return primaries * matrix_float3x3(diagonal: SIMD3<Float>(CP.x, CP.y, CP.z))
 }
